835373a74e03ba85ce013fc9205934114659a399
wataBhata <91080238+wataBhata@users.noreply.github.com>
2022-02-14 12:36:13 -0500

    Added merge-split functions to empanada as dock widgets
    Split, split-by-distance, merge, jump to label, delete labels

--- empanada_napari/__init__.py
+++ empanada_napari/__init__.py
@@ -6,9 +6,16 @@ except ImportError:
 from ._slice_inference import slice_dock_widget
 from ._volume_inference import volume_dock_widget
 from ._register_model import register_model_dock_widget
+from ._merge_split_widget import delete_labels_widget, split_labels_widget, merge_labels_widget, jump_to_label_widget, split_by_dist
 
 __all__ = [
     'slice_dock_widget',
     'volume_dock_widget',
-    'register_model_dock_widget'
+    'register_model_dock_widget',
+    'napari_experimental_provide_function',
+    'merge_labels_widget',
+    'split_labels_widget',
+    'jump_to_label_widget',
+    'split_by_dist',
+    'delete_labels_widget'
 ]
