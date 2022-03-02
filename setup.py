36177d08cb574049633ebad447e20faf878e488f
conradry <conradrw@nih.gov>
2022-03-02 10:42:40 -0500

    deployment ready

--- setup.py
+++ setup.py
@@ -1,45 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from setuptools import setup
 
-from setuptools import setup, find_packages
 
-
-install_deps = [
-    'napari',
-    'napari-plugin-engine>=0.1.4',
-    'imagecodecs',
-    'albumentations',
-    'empanada'
-]
-
-setup(
-    name='empanada-napari',
-    author='Ryan Conrad',
-    author_email='conradrw@nih.gov',
-    license='BSD-3',
-    url='https://github.com/volume-em/empanada-napari',
-    description='Algorithms for Panoptic Segmentation of organelles in EM',
-    packages=find_packages(),
-    python_requires='>=3.7',
-    use_scm_version=True,
-    install_requires=install_deps,
-    dependency_links = ['https://github.com/volume-em/MitoNet#egg=empanada'],
-    setup_requires=['setuptools_scm', 'pytest-runner'],
-    tests_require=['pytest', 'pytest-qt'],
-    include_package_data=True,
-    classifiers=[
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: BSD License',
-        'Framework :: napari',
-    ],
-    entry_points={
-        'napari.plugin': [
-            'empanada-napari = empanada_napari',
-        ],
-    },
-)
+# https://github.com/pypa/setuptools_scm
+use_scm = {"write_to": "empanada_napari/_version.py"}
+setup(use_scm_version=use_scm)
