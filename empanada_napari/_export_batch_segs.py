77062f876f513cfebdd8a895b0b59c1dec6fdd41
barrymg <madeline.barry9@gmail.com>
2024-01-29 14:08:06 -0500

    Added folder naming option and export grayscale option.

--- empanada_napari/_export_batch_segs.py
+++ empanada_napari/_export_batch_segs.py
@@ -26,16 +26,35 @@ def export_batch_segs():
     @magicgui(
         call_button='Export labels',
         layout='vertical',
+        dataset_name=dict(widget_type='LineEdit', value='', label='Folder name',
+                          tooltip='Name to use for the dataset folder, creates a directory in the Save Directory with this name, it appends if directory already exists.'),
         save_dir=dict(widget_type='FileEdit', value='', label='Save directory', mode='d', tooltip='Directory in which to save segmentations'),
-        export_type=dict(widget_type='RadioButtons', choices=list(save_ops.keys()), value=list(save_ops.keys())[0], label='Export type', tooltip='Exports segmentations as individual 2D images or a single 3d image.')
+        export_type=dict(widget_type='RadioButtons', choices=list(save_ops.keys()), value=list(save_ops.keys())[0], label='Export type', tooltip='Exports segmentations as individual 2D images or a single 3d image.'),
+        grayscale=dict(widget_type='CheckBox', value=False, label='Export grayscale image', tooltip='Convert to grayscale'),
     )
     def widget(
         viewer: napari.viewer.Viewer,
         image_layer: Image,
         labels_layer: Labels,
+        export_type: str,
+        dataset_name: str,
         save_dir: str,
-        export_type: str
+        grayscale: bool,
     ):
+        assert dataset_name, "Must provide a dataset name!"
+        assert len(viewer.dims.order) <= 3, "Please use 'Save training patches' instead."
+
+        outdir = os.path.join(save_dir, dataset_name)
+        if not os.path.isdir(outdir):
+            os.makedirs(outdir, exist_ok=True)
+            print('Created directory', outdir)
+        else:
+            print('Adding images to existing directory', outdir)
+
+        if grayscale:
+            os.makedirs(os.path.join(outdir, 'images'), exist_ok=True)
+        os.makedirs(os.path.join(outdir, 'masks'), exist_ok=True)
+
         export_option = save_ops[export_type]
         image = image_layer.data
         mask = labels_layer.data
@@ -51,30 +70,42 @@ def export_batch_segs():
                 ]
             else:
                 zpad = math.ceil(math.log(len(image), 10))
-                imnames = [str(n).zfill(zpad) + '.tiff' for n in range(len(image))]
+                imnames = [image_layer.name + '_' + str(n).zfill(zpad) + '.tiff' for n in range(len(image))]
+                # imnames = [str(n).zfill(zpad) + '.tiff' for n in range(len(image))]
 
             if export_option == '3D image':
                 # Creates a 3D image or 2D stack of images from the label image layer
                 i, h, w = image.shape
                 seg_stack = np.squeeze(mask[:i, :h, :w]).astype(np.int32)
-                imname = labels_layer.name + '.tiff'
-                io.imsave(os.path.join(save_dir, imname), seg_stack, check_contrast=False)
+                imname = image_layer.name + '.tiff'
+
+                if grayscale:
+                    img_stack = np.squeeze(image[:i, :h, :w])
+                    io.imsave(os.path.join(outdir, f'images/{imname}'), img_stack, check_contrast=False)
+                io.imsave(os.path.join(outdir, f'masks/{imname}'), seg_stack, check_contrast=False)
 
             else:
-
-                for i in range(len(mask)):
+                for i in range(len(image)):
                     imname = imnames[i]
                     if isinstance(image, da.Array):
                         h, w = image[i].compute().shape
                     else:
                         h, w = image[i].shape
 
+                    img = np.squeeze(image[i, :h, :w])
                     seg = np.squeeze(mask[i, :h, :w]).astype(np.int32)
-                    io.imsave(os.path.join(save_dir, imname), seg, check_contrast=False)
+
+                    if grayscale:
+                        io.imsave(os.path.join(outdir, f'images/{imname}'), img, check_contrast=False)
+                    io.imsave(os.path.join(outdir, f'masks/{imname}'), seg, check_contrast=False)
 
         else:
             imname = image_layer.name + '.tiff'
-            io.imsave(os.path.join(save_dir, imname), mask.astype(np.int32), check_contrast=False)
+            if grayscale:
+                #imname = image_layer.name + '.tiff'
+                io.imsave(os.path.join(outdir, f'images/{imname}'), image, check_contrast=False)
+            io.imsave(os.path.join(outdir, f'masks/{imname}'), mask.astype(np.int32), check_contrast=False)
+
 
         print('Segmentations exported!')
 
