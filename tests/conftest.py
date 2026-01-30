dc2978f8164fb6275a861a0cf8fe51ac1fe40390
M-A-Demir <mariam.demir@stfc.ac.uk>
2026-01-30 16:26:56 +0000

    Updated missing test dependencies, added conftest.py to setup env configurations for running tests headlessly/without OpenGL

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import os
+
+'''Set environmental variables for integration & benchmark tests to run headlessly/without OpenGL'''
 os.environ["QT_QPA_PLATFORM"] = "offscreen"
 # os.environ.pop("DISPLAY", None)
 os.environ.setdefault("LIBGL_ALWAYS_SOFTWARE", "1")
